427ff17e74f5d2e27130cb080e26f1fceab2ea91
Lacie <dark830917@gmail.com>
2019-12-18 23:45:20 +0800

    分享和儲存功能

--- DrawDraw/ViewController.swift
+++ DrawDraw/ViewController.swift
@@ -21,9 +21,41 @@ class ViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+//        takeScreenshot()
+        test()
+    
+    }
+    
     @IBAction func clearButton(_ sender: UIButton) {
         canvas.clearCanvas()
+    }
+    
+    func takeScreenshot(_ shouldSave: Bool = true) {
+        var screenshotImage :UIImage?
+        let layer = UIApplication.shared.keyWindow!.layer
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+        guard let context = UIGraphicsGetCurrentContext() else {return }
+        layer.render(in:context)
+        screenshotImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        if let image = screenshotImage, shouldSave {
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
+    }
+    
+    func test() {
+        
+          let renderer = UIGraphicsImageRenderer(size: canvas.bounds.size)
+           let image = renderer.image(actions: { (context) in
+              canvas.drawHierarchy(in: canvas.bounds, afterScreenUpdates: true)
+        })
+           let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+           present(activityViewController, animated: true, completion: nil)
+        
+        
     }
     
 }
