c4e018b375e8decdff919cdddb221bd47d72c834
Lacie <dark830917@gmail.com>
2020-01-17 10:44:10 +0800

    把 activityVC 打包起來

--- DrawDraw/Controller/PaintingViewController.swift
+++ DrawDraw/Controller/PaintingViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class PaintingViewController: UIViewController {
 
+    var colorsArray: [UIColor] = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 1, green: 0.4059419876, blue: 0.2455089305, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 1, green: 0.4059419876, blue: 0.2455089305, alpha: 1), #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.3823936913, green: 0.8900789089, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 0.4528176247, blue: 0.4432695911, alpha: 1), #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1), #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)]
+    
     @IBOutlet weak var buttomView: UIView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var selectColorView: UIView!
@@ -49,7 +51,7 @@ class PaintingViewController: UIViewController {
     
     @IBAction func clearCanvasButtonPressed(_ sender: UIButton) {
         
-        showAlert(alertTitle: "Do you want to clear the canvas?", alertMessage: nil, [alertAction("No", .default, nil), alertAction("Yes", .destructive, { [weak self] (UIAlertAction) in
+        showAlert(alertTitle: "Do you want to clear the canvas?", alertMessage: nil, [alertAction("No", .default, nil), alertAction("Yes", .destructive, { [weak self] (_) in
             
             guard let self = self else { return }
             self.canvas.clearCanvas()
@@ -83,13 +85,7 @@ extension PaintingViewController {
         let shareImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        let activityVC = UIActivityViewController(activityItems: [shareImage!], applicationActivities: [])
-        
-        activityVC.excludedActivityTypes = [.assignToContact, .addToReadingList, .openInIBooks, .markupAsPDF, .postToVimeo, .postToWeibo, .postToFlickr, .postToTwitter]
-        
-        present(activityVC, animated: true, completion: nil)
-        
-        activityVC.completionWithItemsHandler = { [weak self] activity, completed, items, error in
+        showActivityVC([shareImage!]) { [weak self] (activityType, completed, items, error) in
             
             guard let self = self else { return }
             
@@ -97,9 +93,19 @@ extension PaintingViewController {
                 print(error ?? "User canceled sharing.")
                 return
             }
-            
-            self.showAlert(alertTitle: "Succeed!", alertMessage: "Successfully shard!", [self.alertAction("OK", .default, nil)])
+            self.showAlert(alertTitle: "Succeed!", alertMessage: "Successfully completed!", [self.alertAction("OK", .default, nil)])
         }
+    }
+    
+    private func showActivityVC(_ activityItems: [Any], _ handler: UIActivityViewController.CompletionWithItemsHandler?) {
+        
+        let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        
+        activityVC.excludedActivityTypes = [.assignToContact, .message, .mail, .addToReadingList, .openInIBooks, .markupAsPDF, .postToVimeo, .postToWeibo, .postToFlickr]
+        
+        present(activityVC, animated: true, completion: nil)
+        
+        activityVC.completionWithItemsHandler = handler
     }
     
     private func showAlert(alertTitle: String, alertMessage: String?, _ actions: [UIAlertAction]) {
